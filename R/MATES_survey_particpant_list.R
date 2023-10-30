6f166234a47b26a26337829bc2f85be5b59483e0
KyleMorrison99 <106417908+KyleMorrison99@users.noreply.github.com>
2023-10-30 15:16:44 +1100

    split participants

--- R/MATES_survey_particpant_list.R
+++ R/MATES_survey_particpant_list.R
@@ -14,3 +14,29 @@ data <- data %>%
                                                            
 write.csv(data, "data/MATES_participant_author_list.csv")
 
+df <- read.csv("data/MATES_participant_author_list.csv")
+
+
+# Number of rows in the data frame
+total_rows <- nrow(df)
+
+# Number of rows per chunk
+chunk_size <- 400
+
+# Calculate the number of chunks needed
+num_chunks <- ceiling(total_rows / chunk_size)
+
+# Loop to create each chunk
+for (i in 1:num_chunks) {
+  start_row <- ((i - 1) * chunk_size) + 1
+  end_row <- min(i * chunk_size, total_rows)
+  
+  # Subset the data frame
+  subset_df <- df[start_row:end_row, ]
+  
+  # Create the file name
+  file_name <- paste("MATES_participants_", start_row, "_to_", end_row, ".csv", sep = "")
+  
+  # Write the CSV file
+  write_csv(subset_df, paste("C:/Users/khtmo/OneDrive - UNSW/University/PhD_Chapters/5_mates/MATES_Analysis/data", file_name, sep = ""))
+}
